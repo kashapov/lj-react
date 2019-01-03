d61ab4551ef26827270ddb1c237f8fafd2592b22
Andrey Kashapov <kashapovandrey@gmail.com>
2019-01-03 21:20:10 +0300

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,21 @@ import ArticleList from "./ArticleList";
 import UserForm from "./UserForm";
 import Filters from "./Filters";
 import Counter from "./Counter";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import "react-select/dist/react-select.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Counter />
-        <UserForm />
-        <Filters articles={[]} />
-        <ArticleList />
-      </div>
+      <Router>
+        <div>
+          <UserForm />
+          <Route path="/counter" component={Counter} />
+          <Route path="/filters" component={Filters} />
+          <Route path="/articles" component={ArticleList} />
+        </div>
+      </Router>
     );
   }
 }
