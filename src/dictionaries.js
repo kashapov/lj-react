f18072bd9ebc4ac66b52135fe7283d6dbc836b7f
Andrey Kashapov <kashapovandrey@gmail.com>
2019-01-11 13:38:02 +0300

    add dict

---
+++ src/dictionaries.js
@@ -0,0 +1,17 @@
+export const en = {
+  "delete me": "delete me",
+  "show comments": "show comments",
+  "hide comments": "hide comments",
+  "No comments yet": "No comments yet",
+  Loading: "Loading"
+};
+
+export const ru = {
+  "delete me": "удалить статью",
+  "show comments": "показать комментарии",
+  "hide comments": "спрятать комментарии",
+  "No comments yet": "Пока нет комментариев",
+  Loading: "Загрузка"
+};
+
+export default { ru, en };
